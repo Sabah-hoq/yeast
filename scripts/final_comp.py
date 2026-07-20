40ea218fbd9234715aec658c269fce67ff3d4fbe
Sabah-hoq <skhondaker@wisc.edu>
2026-07-20 16:50:06 -0500

    final comparison dataframe

---
+++ scripts/final_comp.py
@@ -0,0 +1,121 @@
+import os
+import argparse
+from pathlib import Path
+import polars as pl
+from string__downloader import download_string_data  
+from load_data import load_data
+
+def normalize_string_id_column(lf: pl.LazyFrame) -> pl.LazyFrame:
+    string_col = "string_protein_id" if "string_protein_id" in lf.columns else "#string_protein_id"
+    return lf.rename({string_col: "string_id"})
+
+
+def load_and_map_data(data_dir_path):
+    data_dir = Path(data_dir_path)
+    
+    print("Scanning data sources (local files and cached web streams)...")
+    
+    df2 = download_string_data(data_id="protein.physical.links.detailed", cols_to_clean=["protein1", "protein2"])
+    string_aliases = download_string_data(data_id="protein.aliases")
+    string_info = download_string_data(data_id="protein.info")
+    
+    print(type(string_aliases))
+
+    pairs, confidences = load_data("data/")
+
+    print("Building unique protein lists and dictionary map...")
+    unique_proteins = (pl.concat([
+        pairs.select(pl.col("af3_id1").alias("protein_id")),
+        pairs.select(pl.col("af3_id2").alias("protein_id"))
+    ]).drop_nulls().unique().sort("protein_id"))
+
+
+    base_map = pl.concat([
+        pairs.select([
+            pl.col("af3_id1").alias("uniprot_id"),
+            pl.col("michaelis2023:Source Gene names (SGD/UniProt-primary or ordered locus)").alias("gene_name"),
+        ]),
+        pairs.select([
+            pl.col("af3_id2").alias("uniprot_id"),
+            pl.col("michaelis2023:Target Gene names  (SGD/UniProt-primary or ordered locus)").alias("gene_name"),
+        ])
+    ]).drop_nulls().unique(subset=["uniprot_id"])
+
+    already_mapped_ids = base_map.select("uniprot_id").collect().get_column("uniprot_id").to_list()
+    need_api = unique_proteins.filter(pl.col("protein_id").is_in(already_mapped_ids).not_())
+
+    clean_aliases = (
+        string_aliases
+        .rename({"alias": "protein_id"})
+        .pipe(normalize_string_id_column)
+        .with_columns(pl.col("protein_id").str.to_lowercase())
+        .filter(pl.col("source").str.contains("UniProt"))
+    )
+
+    matched_missing = (
+        need_api.join(clean_aliases, on="protein_id", how="inner")
+        .select([pl.col("protein_id").alias("uniprot_id"), pl.col("string_id")])
+        .unique()
+    )
+
+    clean_info = normalize_string_id_column(string_info)
+    full_matches = matched_missing.join(clean_info, on="string_id", how="inner") 
+    #need .select([pl.col("uniprot_id"),pl.col("string_id"), pl.col("preferred_name").alias("string_gene_name"),pl.col("annotation")
+
+    df_matches_unique = full_matches.unique(subset=['uniprot_id'], keep='first').collect() 
+    id_map_dict = dict(zip(df_matches_unique['uniprot_id'], df_matches_unique['string_id']))
+
+    print("Aligning interaction networks and merging scores...")
+    pairs_2 = pairs.select([
+        pl.col("af3_id1").alias("protein1"), pl.col("af3_id2").alias("protein2"),
+        pl.col("chain_pair_iptm_best"), pl.col("chain_pair_iptm_mean")
+    ]).drop_nulls().unique()
+
+    df_alphafold_mapped = pairs_2.with_columns([
+        pl.col("protein1").replace_strict(id_map_dict, default=None).alias("string_id1"),
+        pl.col("protein2").replace_strict(id_map_dict, default=None).alias("string_id2")
+    ]).drop_nulls(subset=["string_id1", "string_id2"])
+
+    df_all_mapped = df_alphafold_mapped.with_columns(
+        pl.min_horizontal("string_id1", "string_id2").alias("pair_key1"),
+        pl.max_horizontal("string_id1", "string_id2").alias("pair_key2")
+    )
+
+    df2_2 = df2.select(["protein1", "protein2", "combined_score"]).unique()
+    df_string_ordered = df2_2.with_columns(
+        pl.min_horizontal("protein1", "protein2").alias("pair_key1"),
+        pl.max_horizontal("protein1", "protein2").alias("pair_key2")
+    )
+    df_string_unique_pairs = df_string_ordered.select(["pair_key1", "pair_key2", "combined_score"]).unique(subset=["pair_key1", "pair_key2"])
+
+    df_final_comparison = df_all_mapped.join(
+        df_string_unique_pairs, on=["pair_key1", "pair_key2"], how="left"
+    ).drop("pair_key1", "pair_key2")
+
+    return df_final_comparison.collect()
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Global Score Pipeline Wrapper")
+    parser.add_argument("--data-dir", type=str, required=True, help="Path to local data folder (with summary_pairs.parquet)")
+    parser.add_argument("--output", type=str, default="final_comp.csv", help="output filename")
+    args = parser.parse_args()
+    
+    print("Running mapping pipeline standalone...")
+
+    final_df = load_and_map_data(args.data_dir)
+    
+    data_folder = Path(args.data_dir)
+    csv_path = data_folder / args.output
+    final_df.write_csv(csv_path)
+    print(f"CSV file saved at: {csv_path} (Total rows: {final_df.height})")
+
+    repo_root = data_folder.parent
+    folder_path = repo_root / "notebooks/PR_ROC"
+    folder_path.mkdir(parents=True, exist_ok=True)
+    csv_file_path = folder_path / "final_comp.csv"
+
+    final_df.write_csv(csv_file_path)
+    print(f"CSV saved successfully at: {csv_file_path}")
+
